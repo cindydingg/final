c1ef2d1adf5ac2c573c73c508332206d1b67b32c
rponc <166068876+rponc@users.noreply.github.com>
2024-05-23 22:41:11 -0700

    MainMenuUpdate
    changed background added function to buttons

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,4 +9,14 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+
+    public void options()
+    {
+
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
 }
