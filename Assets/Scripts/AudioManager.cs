cdc9b5ab23296b5446790d07daa34a083837fc15
rponc <166068876+rponc@users.noreply.github.com>
2024-05-29 14:53:28 -0700

    Audio
    added audio asset packs and audiofiles to main menu, level 1 and level 2

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("----- Audio Source -----")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("----- Audio Clip -----")]
+    public AudioClip background;
+    public AudioClip pickup;
+
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+     
+}
